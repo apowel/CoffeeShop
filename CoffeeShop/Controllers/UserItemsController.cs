a5525d9977ecff3aec89301c9111d9a680e3f5fe
Austin <austingpowel@gmail.com>
2020-04-20 09:18:18 -0400

    Cleaned up look of some buttons.

--- CoffeeShop/Controllers/UserItemsController.cs
+++ CoffeeShop/Controllers/UserItemsController.cs
@@ -58,14 +58,14 @@ namespace CoffeeShop.Controllers
                 return NotFound();
             }
 
-            var userItems = await _context.UserItems
-                .FirstOrDefaultAsync(m => m.UserItemId == id);
-            if (userItems == null)
+            var items = await _context.Items
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (items == null)
             {
                 return NotFound();
             }
 
-            return View(userItems);
+            return View(items);
         }
 
         // GET: UserItems/Create
@@ -85,57 +85,6 @@ namespace CoffeeShop.Controllers
             {
                 _context.Add(userItems);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(userItems);
-        }
-
-        // GET: UserItems/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var userItems = await _context.UserItems.FindAsync(id);
-            if (userItems == null)
-            {
-                return NotFound();
-            }
-            return View(userItems);
-        }
-
-        // POST: UserItems/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("UserItemId,UserId,ItemId")] UserItems userItems)
-        {
-            if (id != userItems.UserItemId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(userItems);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UserItemsExists(userItems.UserItemId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
                 return RedirectToAction(nameof(Index));
             }
             return View(userItems);
